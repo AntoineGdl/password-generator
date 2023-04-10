98474b21690da057c71ff6dc6b9805b2afb8847c
AntoineGdl <130215991+AntoineGdl@users.noreply.github.com>
2023-04-10 16:48:28 +0200

    Version finale du Main
    Ajout d'un menu plus complet

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 import fonctions as fct
 
 def main():
-    print("")
-    choix = int(input("Un mot de passe avec quel niveau de difficulté voulez-vous créer ?"))
-    if choix == 1:
-        print(fct.mdpNiv1(longueur=int(input("De quelle longueur souhaitez vous votre mot de passe ?"))))
-    elif choix == 2:
-        return fct.mdpNiv2(longueurMot=int(input("De quelle longueur souhaitez-vous votre mot de passe ?")),longueurChiffre=int(input("Combien de chiffre(s) souhaitez-vous ?")))
-    elif choix == 3:
-        return fct.mdpNiv3(longueurMot=int(input("De quelle longueur souhaitez-vous votre mot de passe ?")),lettresMaj=int(input("Combien de lettre(s) en majuscules souhaitez-vous ?")),lettresMin=int(input("Combien de lettre(s) en minuscules souhaitez-vous ?")),nbChiffres=int(input("Combien de chiffre(s) souhaitez vous ?")))
-    elif choix == 4:
-        return fct.mdpNiv4(longueurMot=int(input("De quelle longueur souhaitez-vous votre mot de passe ?")),lettresMaj=int(input("Combien de lettre(s) en majuscules souhaitez-vous ?")),lettresMin=int(input("Combien de lettre(s) en minuscules souhaitez-vous ?")),nbChiffres=int(input("Combien de chiffre(s) souhaitez vous ?")),longueurCarS=int(input("Combien de Caractères Spéciaux souhaitez-vous ?")))
-    else:
-        print("Vous devez saisir un nombre entre 1 et 4 inclus")
+    choix = ""
+    while choix != -1:
+        choix = int(input("Un mot de passe avec quel niveau (1 à 4 inclus) de difficulté voulez-vous créer ? (-1 pour quitter)"))
+        if choix == 1:
+            print(fct.mdpNiv1(longueur=int(input("Combien de caractères souhaitez-vous dans votre mot de passe ?"))))
+        elif choix == 2:
+            print(fct.mdpNiv2(longueurMot=int(input("Combien de minuscules souhaitez-vous dans votre mot de passe ?")),longueurChiffre=int(input("Combien de chiffre(s) souhaitez-vous ?"))))
+        elif choix == 3:
+            print(fct.mdpNiv3(longueurMot=int(input("Combien de minuscules souhaitez-vous dans votre mot de passe ?")),lettresMaj=int(input("Combien de lettre(s) en majuscules souhaitez-vous ?")),nbChiffres=int(input("Combien de chiffre(s) souhaitez vous ?"))))
+        elif choix == 4:
+            print(fct.mdpNiv4(longueurMot=int(input("Combien de minuscules souhaitez-vous dans votre mot de passe ?")),lettresMaj=int(input("Combien de lettre(s) en majuscules souhaitez-vous ?")),nbChiffres=int(input("Combien de chiffre(s) souhaitez vous ?")),longueurCarS=int(input("Combien de Caractères Spéciaux souhaitez-vous ?"))))
+    return "Merci d'avoir utilisé mon programme"
     
 print(main())
